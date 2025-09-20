4e58081caa01c3d8d1da6b5281293e0b6500ee2e
cmh02 <cmh0201@auburn.edu>
2025-09-20 00:56:52 -0500

    Added toggleable hashing and support for other types of hashing

--- model-src/mcalib/MCA_DataPrepare.py
+++ model-src/mcalib/MCA_DataPrepare.py
@@ -53,13 +53,14 @@ CLASS DEFINITION
 '''
 
 class McaDataPrepare:
-	def __init__(self, inputFolderPath: str, outputMode: McaOutputMode=McaOutputMode.ALL, storageMode: McaStorageMode=McaStorageMode.NONE):
+	def __init__(self, inputFolderPath: str, outputMode: McaOutputMode=McaOutputMode.ALL, storageMode: McaStorageMode=McaStorageMode.NONE, hashMode: McaHashMode=McaHashMode.NONE):
 
 		# Save instance configuration
 		self.inputFolderPath = inputFolderPath
 		self.dataFolderPath = os.path.dirname(os.path.dirname(inputFolderPath))
 		self.outputMode = outputMode
 		self.storageMode = storageMode
+		self.hashMode = hashMode
 
 		# Load environment file using python-dotenv
 		load_dotenv(dotenv_path="../env/.env")
@@ -79,22 +80,30 @@ class McaDataPrepare:
 		print(f"-> Data Directory Path: {self.dataFolderPath}")
 		print(f"-> Output Mode: {self.outputMode}")
 		print(f"-> Storage Mode: {self.storageMode}")
+		print(f"-> Hash Mode: {self.hashMode}")
 
 	def prepareData(self):
 
 		# Set up intermediate output paths depending on output type
 		if self.outputMode in (McaOutputMode.PUBLIC, McaOutputMode.ALL):
-			self.anonymizedFolderPath_public = os.path.join(self.dataFolderPath, "anonymized/public/")
-			os.makedirs(self.anonymizedFolderPath_public, exist_ok=True)
+
+			# Create anonymized folder path if hashing is enabled
+			if self.hashMode != McaHashMode.NONE:
+				self.anonymizedFolderPath_public = os.path.join(self.dataFolderPath, "anonymized/public/")
+				os.makedirs(self.anonymizedFolderPath_public, exist_ok=True)
 
 			self.cleanedFolderPath_public = os.path.join(self.dataFolderPath, "cleaned/public/")
 			os.makedirs(self.cleanedFolderPath_public, exist_ok=True)
 
 			self.featurizedFolderPath_public = os.path.join(self.dataFolderPath, "featurized/public/")
 			os.makedirs(self.featurizedFolderPath_public, exist_ok=True)
+
 		if self.outputMode in (McaOutputMode.PRIVATE, McaOutputMode.ALL):
-			self.anonymizedFolderPath_private = os.path.join(self.dataFolderPath, "anonymized/private/")
-			os.makedirs(self.anonymizedFolderPath_private, exist_ok=True)
+
+			# Create anonymized folder path if hashing is enabled
+			if self.hashMode != McaHashMode.NONE:
+				self.anonymizedFolderPath_private = os.path.join(self.dataFolderPath, "anonymized/private/")
+				os.makedirs(self.anonymizedFolderPath_private, exist_ok=True)
 
 			self.cleanedFolderPath_private = os.path.join(self.dataFolderPath, "cleaned/private/")
 			os.makedirs(self.cleanedFolderPath_private, exist_ok=True)
@@ -102,12 +111,13 @@ class McaDataPrepare:
 			self.featurizedFolderPath_private = os.path.join(self.dataFolderPath, "featurized/private/")
 			os.makedirs(self.featurizedFolderPath_private, exist_ok=True)
 
-		# Set up output folder path (output folder always gets made)
-		self.outputFolderPath_public = os.path.join(self.dataFolderPath, "prepared/public/")
-		os.makedirs(self.outputFolderPath_public, exist_ok=True)
+		# Set up output folder path unless output mode is NONE or INSTANCE
+		if self.outputMode not in (McaOutputMode.NONE, McaOutputMode.FINAL):
+			self.outputFolderPath_public = os.path.join(self.dataFolderPath, "prepared/public/")
+			os.makedirs(self.outputFolderPath_public, exist_ok=True)
 
-		self.outputFolderPath_private = os.path.join(self.dataFolderPath, "prepared/private/")
-		os.makedirs(self.outputFolderPath_private, exist_ok=True)
+			self.outputFolderPath_private = os.path.join(self.dataFolderPath, "prepared/private/")
+			os.makedirs(self.outputFolderPath_private, exist_ok=True)
 
 		# Get the names of all input files using glob
 		inputDataFiles = glob(os.path.join(self.inputFolderPath, "**", "*.csv"), recursive=True)
@@ -121,20 +131,26 @@ class McaDataPrepare:
 			# Read the input CSV file
 			df = pd.read_csv(filePath)
 
-			# Anonymize the UUID column (UUID -> hash(PEPPER + UUID))
-			df['UUID'] = [hashlib.sha256(f"{self.MCA_PEPPERKEY}:{uuid}".encode()).hexdigest() for uuid in df['UUID']]
+			# If hashing is enabled, anonymize the UUID column (UUID -> hash(PEPPER + UUID))
+			if self.hashMode != McaHashMode.NONE:
 
-			# If configured, create private output path and save dataframe to path
-			if self.outputMode in (McaOutputMode.PRIVATE, McaOutputMode.ALL):
-				outputFilePath = os.path.join(self.anonymizedFolderPath_private, dataRelativeFilePath)
-				os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
-				df.to_csv(outputFilePath, index=False)
+				# Hash the UUID's using configured hash mode
+				if self.hashMode == McaHashMode.SHA256:
+					df['UUID'] = [hashlib.sha256(f"{self.MCA_PEPPERKEY}:{uuid}".encode()).hexdigest() for uuid in df['UUID']]
+				else:
+					raise ValueError(f"Hash Mode Not Implemented: {self.hashMode}")
 
-			# If configured, create public output path and save dataframe without UUID's to path
-			if self.outputMode in (McaOutputMode.PUBLIC, McaOutputMode.ALL):
-				outputFilePath = os.path.join(self.anonymizedFolderPath_public, dataRelativeFilePath)
-				os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
-				df.drop(columns=['UUID']).to_csv(outputFilePath, index=False)
+				# If configured, create private hashed output path and save dataframe to path
+				if self.outputMode in (McaOutputMode.PRIVATE, McaOutputMode.ALL):
+					outputFilePath = os.path.join(self.anonymizedFolderPath_private, dataRelativeFilePath)
+					os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
+					df.to_csv(outputFilePath, index=False)
+
+				# If configured, create public hashed output path and save dataframe without UUID's to path
+				if self.outputMode in (McaOutputMode.PUBLIC, McaOutputMode.ALL):
+					outputFilePath = os.path.join(self.anonymizedFolderPath_public, dataRelativeFilePath)
+					os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
+					df.drop(columns=['UUID']).to_csv(outputFilePath, index=False)
 
 			# Replace <none> with NULL
 			df.replace("<none>", pd.NA, inplace=True)
@@ -196,13 +212,13 @@ class McaDataPrepare:
 			# Remove any extra text from balance column
 			df["balance"] = df["balance"].replace({"dollars": "", "dollar": "", "money": "", "Dollars": "", "Dollar": "", "Money": ""}, regex=True)
 
-			# If configured, create private output path and save dataframe to path
+			# If configured, create private cleaned output path and save dataframe to path
 			if self.outputMode in (McaOutputMode.PRIVATE, McaOutputMode.ALL):
 				outputFilePath = os.path.join(self.cleanedFolderPath_private, dataRelativeFilePath)
 				os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
 				df.to_csv(outputFilePath, index=False)
 
-			# If configured, create public output path and save dataframe without UUID's to path
+			# If configured, create public cleaned output path and save dataframe without UUID's to path
 			if self.outputMode in (McaOutputMode.PUBLIC, McaOutputMode.ALL):
 				outputFilePath = os.path.join(self.cleanedFolderPath_public, dataRelativeFilePath)
 				os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
@@ -221,13 +237,13 @@ class McaDataPrepare:
 			df.replace([np.inf, -np.inf], np.nan, inplace=True)
 			df.fillna(0, inplace=True)
 
-			# If configured, create private output path and save dataframe to path
+			# If configured, create private featurized output path and save dataframe to path
 			if self.outputMode in (McaOutputMode.PRIVATE, McaOutputMode.ALL):
 				outputFilePath = os.path.join(self.featurizedFolderPath_private, dataRelativeFilePath)
 				os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
 				df.to_csv(outputFilePath, index=False)
 
-			# If configured, create public output path and save dataframe without UUID's to path
+			# If configured, create public featurized output path and save dataframe without UUID's to path
 			if self.outputMode in (McaOutputMode.PUBLIC, McaOutputMode.ALL):
 				outputFilePath = os.path.join(self.featurizedFolderPath_public, dataRelativeFilePath)
 				os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
