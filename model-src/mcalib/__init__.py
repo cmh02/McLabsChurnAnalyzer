0c02af5f09ad6f3025cea0f3ee15a87e827f2fd4
cmh02 <cmh0201@auburn.edu>
2025-09-27 00:52:00 -0500

    Added init for library to make imports easier

---
+++ model-src/mcalib/__init__.py
@@ -0,0 +1,10 @@
+'''
+MCA Library
+Author: @cmh02
+'''
+
+# Specific imports
+from .MCA_DataPrepare import McaDataPrepare, McaOutputMode, McaStorageMode, McaHashMode
+
+# Define imports for *
+__all__ = ["McaDataPrepare", "McaOutputMode", "McaStorageMode", "McaHashMode"]
