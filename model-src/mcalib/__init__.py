f0b629e65e8332e51bd15c595602d975d1869692
cmh02 <cmh0201@auburn.edu>
2025-09-27 15:27:43 -0500

    Creating predictions module

--- model-src/mcalib/__init__.py
+++ model-src/mcalib/__init__.py
@@ -7,6 +7,7 @@ Author: @cmh02
 from .MCA_Enum import McaOutputMode, McaStorageMode, McaHashMode
 from .MCA_DataPrepare import McaDataPrepare
 from .MCA_DataCombine import McaDataCombine
+from .MCA_Predictor import McaPredictor
 
 # Define imports for *
-__all__ = ["McaOutputMode", "McaStorageMode", "McaHashMode", "McaDataPrepare", "McaDataCombine"]
+__all__ = ["McaOutputMode", "McaStorageMode", "McaHashMode", "McaDataPrepare", "McaDataCombine", "McaPredictor"]
