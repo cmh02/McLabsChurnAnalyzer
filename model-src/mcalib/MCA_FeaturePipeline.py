307a9876a61f0856ab8b1a8d8579a4cece42dd10
cmh02 <cmh0201@auburn.edu>
2025-10-08 19:19:47 -0500

    New Feature Engineering Pipeline module with static architecture

---
+++ model-src/mcalib/MCA_FeaturePipeline.py
@@ -0,0 +1,74 @@
+'''
+MCA DataCombine Module
+Author: @cmh02
+
+This module will provide a Feature Engineering pipeline for cross-timestamp data preparation tasks:
+
+For the target variable, we observe the following classes:
+- Inactive (0): Player has not been active for either timestamp
+- Recovered (1): Player was inactive in t1, but active in t2
+- Churned (2): Player was active in t1, but inactive in t2
+- Active (3): Player has been active in both timestamps
+'''
+
+'''
+MODULE/PACKAGE IMPORTS
+'''
+
+# Data
+import numpy as np
+import pandas as pd
+
+'''
+CLASS DEFINITION
+'''
+
+class McaFeaturePipeline:
+	'''
+	MCA Feature Engineering Class
+	'''
+
+	@staticmethod
+	def combineData(currentDf: pd.DataFrame, previousDf: pd.DataFrame) -> pd.DataFrame:
+		'''
+		Combine two dataframes from different timestamps into one dataframe with engineered features.
+
+		Parameters:
+		- currentDf: DataFrame for timestamp 2 (the most up-to-date data timestamp)
+		- previousDf: DataFrame for timestamp 1
+		'''
+
+		# Merge the two dataframes on 'UUID' column
+		df = currentDf.merge(previousDf, on='UUID', how='outer', suffixes=('_t2', '_t1'))
+
+		# Get change in balance
+		df['balance_change'] = np.abs(df['balance_t2'] - df['balance_t1'])
+
+		# Get change in lw metrics
+		df['lw_rev_total_change'] = np.abs(df['lw_rev_total_t2'] - df['lw_rev_total_t1'])
+		df['lw_rev_phase_change'] = np.abs(df['lw_rev_phase_t2'] - df['lw_rev_phase_t1'])
+
+		# Get change in leaderboard metrics
+		df['leaderboard_position_chems_all_change'] = np.abs(df['leaderboard_position_chems_all_t2'] - df['leaderboard_position_chems_all_t1'])
+		df['leaderboard_position_chems_week_change'] = np.abs(df['leaderboard_position_chems_week_t2'] - df['leaderboard_position_chems_week_t1'])
+		df['leaderboard_position_police_all_change'] = np.abs(df['leaderboard_position_police_all_t2'] - df['leaderboard_position_police_all_t1'])
+		df['leaderboard_position_police_week_change'] = np.abs(df['leaderboard_position_police_week_t2'] - df['leaderboard_position_police_week_t1'])
+
+		# Get change in rank metrics
+		df['chemrank_change'] = np.abs(df['chemrank_t2'] - df['chemrank_t1'])
+		df['policerank_change'] = np.abs(df['policerank_t2'] - df['policerank_t1'])
+		df['donorrank_change'] = np.abs(df['donorrank_t2'] - df['donorrank_t1'])
+		df['goldrank_change'] = np.abs(df['goldrank_t2'] - df['goldrank_t1'])
+
+		# Drop unneeded columns
+		df.drop(columns=['active_t1', 'active_t2'], inplace=True)
+
+		# Drop all columns with _t1 suffix
+		t1Cols = [col for col in df.columns if col.endswith('_t1')]
+		df.drop(columns=t1Cols, inplace=True)
+
+		# Rename all columns with _t2 suffix to remove the suffix
+		df.rename(columns=lambda x: x.replace('_t2', ''), inplace=True)
+
+		# Return the combined dataframe
+		return df
